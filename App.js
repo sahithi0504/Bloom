7dcbe78e9156eeae7ad6d33d030293ec62becb0d
Joseph Aycock <josephfelixaycock@outlook.com>
2024-04-07 07:13:46 -0500

    testJoseph

--- App.js
+++ App.js
@@ -6,8 +6,6 @@ import ResultsScreen from "./screens/ResultsScreen";
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/LoginScreen';
 import HomeScreen from './src/HomeScreen';
 
